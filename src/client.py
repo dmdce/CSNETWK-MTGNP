307d650b4738fbc0ce760ddf1f9bf04f444649d8
dmdce <da.real.esguerra@gmail.com>
2026-07-03 23:15:22 +0800

    chore: moved send recv methods to utils.py

--- src/client.py
+++ src/client.py
@@ -1,35 +1,10 @@
 import struct
 import sys
 import socket
-import json
+import utils
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 6700
-
-def send_pdu(client, pdu):
-    # Frame JSON PDU with 4-byte big-endian length prefix
-    payload = json.dumps(pdu).encode('utf-8')
-    header = struct.pack('>I', len(payload))
-    client.sendall(header + payload)
-
-def recv_pdu(client):
-    # Reads amount of bytes in 4-byte prefix
-    header = client.recv(4)
-    if not header:
-        return None
-
-    length = struct.unpack('>I', header)[0]
-    chunks = []
-    bytes_recd = 0
-
-    while bytes_recd < length:
-        chunk = client.recv(min(4096, length - bytes_recd))
-        if not chunk:
-            raise ConnectionError('Socket closed prematurely')
-        chunks.append(chunk)
-        bytes_recd += len(chunk)
-
-    return json.loads(b''.join(chunks).decode('utf-8'))
 
 def start_client(player_id):
     try:
@@ -46,7 +21,7 @@ def start_client(player_id):
             "deck_list": []
         }
 
-        send_pdu(client, ready_pdu)
+        utils.send_pdu(client, ready_pdu)
 
 
     except ConnectionRefusedError:
---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import json
+import struct
+
+def send_pdu(sock, pdu):
+    # Frame JSON PDU with 4-byte big-endian length prefix
+    payload = json.dumps(pdu).encode('utf-8')
+    header = struct.pack('>I', len(payload))
+    sock.sendall(header + payload)
+
+def recv_pdu(sock):
+    # Reads amount of bytes in 4-byte prefix
+    header = sock.recv(4)
+    if not header:
+        return None
+
+    length = struct.unpack('>I', header)[0]
+    chunks = []
+    bytes_recd = 0
+
+    while bytes_recd < length:
+        chunk = sock.recv(min(4096, length - bytes_recd))
+        if not chunk:
+            raise ConnectionError('+ utils.py: Socket closed prematurely')
+        chunks.append(chunk)
+        bytes_recd += len(chunk)
+
+    return json.loads(b''.join(chunks).decode('utf-8'))
